4833264fb26baea863172697b2b57caf803fd6e0
muralisrinivas94-sudo <muralisrinivas94@gmail.com>
2026-02-16 12:48:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,1 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write(b"Hello World from Kubernetes!")
-
-PORT = 5000
-server = HTTPServer(("0.0.0.0", PORT), Handler)
-
-print(f"Server running on port {PORT}")
-server.serve_forever()
+print("Hello from Jenkins Python job")
